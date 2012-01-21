b0fadea36d0353ca715f985879c69256e8fcf01e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-01-21 21:04:57 +0000

    Removed old bootstrap subpackage.

--- core/bootstrap/_delete_audiodb_databases.py
+++
@@ -1,9 +0,0 @@
-from sasha import SASHA
-from sasha.core.wrappers import AudioDB
-
-
-def _delete_audiodb_databases( ):
-
-    for name in SASHA['audioDB']:
-        adb = AudioDB(name)
-        adb.delete( )
