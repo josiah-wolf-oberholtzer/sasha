565b9396c16d1ab6778777b222cd56688b03c852
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-03-23 21:27:24 +0000

    Changed CroppedAudio cropping.

--- plugins/audio/CroppedAudio/CroppedAudio.py
+++ plugins/audio/CroppedAudio/CroppedAudio.py
@@ -20,8 +20,8 @@ class CroppedAudio(_MediaPlugin):
 
     def write(self, **kwargs):
         samples, samplerate = SourceAudio(self).read()
-        start = int(len(samples) * 0.25)
-        stop = int(len(samples) * 0.75)
+        start = int(len(samples) * 0.2)
+        stop = int(len(samples) * 0.9)
         cropped_samples = samples[start:stop]
         snd = audiolab.Sndfile(self.path, 'w', audiolab.Format('aiff'), 1, samplerate)
         snd.write_frames(cropped_samples)
