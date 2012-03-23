9ef99b84504c6b278ba18c1f2f0ed7469a0bbab4
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-03-23 18:38:06 +0000

    Moved exceptions out of core, and into root. Added mediatools.play() to root __init__.

--- __init__.py
+++ __init__.py
@@ -2,15 +2,15 @@ import os
 SASHAROOT = os.path.abspath(os.path.dirname(__file__))
 del os
 
-from core.config import SashaConfig
+from sasha.core.config import SashaConfig
 SASHA = SashaConfig( )
 del SashaConfig
 
-#from sasha.core.domain import Event
-#from sasha.core.domain import Idiom
-#from sasha.core.domain import Instrument
-#from sasha.core.domain import Performer
 from sasha.core.domain import *
 from sasha.core.wrappers import AudioDB
+from sasha.tools.mediatools import play
+
 del core
+del plugins
+del tools
 
