1c9eca1ac994c265c7bdcae4bd38cd3cfab5307b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-01-15 22:39:01 -0800

    Sketched out package boilerplate.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='sasha',
+    version='0.1',
+    install_requires=[
+        'abjad',
+        ],
+    packages=[
+        'sasha',
+        ],
+    )
