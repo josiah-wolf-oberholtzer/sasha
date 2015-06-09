fa7c3670e2c09b7ab8a0eac3641c8ac02f0cf769
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-09 10:01:35 -0700

    Cleaned up PluginGraph.

--- sasha/tools/assettools/PluginGraph.py
+++ sasha/tools/assettools/PluginGraph.py
@@ -4,32 +4,31 @@ import inspect
 
 class PluginGraph(object):
 
-    __slots__ = ('_client', '_graph')
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_client',
+        '_graph',
+        )
+
+    ### INITIALIZER ###
 
     def __init__(self, client):
-        import sasha.plugins
-        from sasha.tools.assettools._Plugin import _Plugin
-
-        self._client = client
-        self._graph = { } 
-
-        plugins = filter(lambda x:
-            hasattr(x, '__bases__') and
-            _Plugin in inspect.getmro(x) and 
-            x.__client_class__ == self.client and
-            not inspect.isabstract(x),
-            [getattr(sasha.plugins, x) for x in dir(sasha.plugins)])
-
         def _build_subtree(target, node, reservoir):
-            '''Loop through reservoir.  If a plugin's __requires__
-            is equal to target, add that plugin as a key in node,
-            and call this function recursively on that new node.'''
-
             for x in reservoir:
                 if x.__requires__ == target:
-                    node[x] = { }
+                    node[x] = {}
                     _build_subtree(x, node[x], reservoir)
-
+        from sasha.tools import assettools
+        from sasha.tools.assettools._Plugin import _Plugin
+        self._client = client
+        self._graph = {}
+        plugins = filter(lambda x:
+            hasattr(x, '__bases__') and
+            _Plugin in inspect.getmro(x) and
+            x.__client_class__ == self.client and
+            not inspect.isabstract(x),
+            [getattr(assettools, x) for x in dir(assettools)])
         _build_subtree(None, self._graph, plugins)
 
     ### PUBLIC ATTRIBUTES ###
@@ -45,13 +44,11 @@ class PluginGraph(object):
     ### PUBLIC METHODS ###
 
     def in_order(self):
-
         def _depth_first(node):
-            result = [ ]
+            result = []
             for k, v in node.iteritems():
                 result.append(k)
                 if v:
                     result.extend(_depth_first(v))
             return result
-
         return tuple(_depth_first(self.graph))
