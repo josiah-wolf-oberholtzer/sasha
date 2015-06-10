2edeed3d2ce4974f445132ce617a92cea566947b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-10 16:43:04 -0700

    Added comments.

--- sasha/tools/__init__.py
+++ sasha/tools/__init__.py
@@ -1,5 +1,7 @@
+# System tools must be imported first.
 from sasha.tools import systemtools
 
+# Remaining tools packages can then be imported safely.
 from sasha.tools import analysistools
 from sasha.tools import assettools
 from sasha.tools import collectiontools
