a44848f9fa41f5a293f4ac1b270848c596a067d4
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-10 18:17:03 -0700

    Removed unused imports.

--- sasha/tools/__init__.py
+++ sasha/tools/__init__.py
@@ -4,7 +4,5 @@ from sasha.tools import systemtools
 # Remaining tools packages can then be imported safely.
 from sasha.tools import analysistools
 from sasha.tools import assettools
-from sasha.tools import collectiontools
-from sasha.tools import diagramtools
 from sasha.tools import mediatools
 from sasha.tools import wrappertools
