0b0d38b0b5228de687348327ef5c356a2a0bf79d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-12 16:49:44 -0700

    Removed ImmutableDictionary.

--- sasha/tools/systemtools/ImmutableDictionary.py
+++
@@ -1,9 +0,0 @@
-class ImmutableDictionary(dict):
-
-    ### SPECIAL METHODS ###
-
-    def __delitem__(self, *args):
-        raise AttributeError('objects are immutable: "%s".' % self.__class__.__name__)
-
-    def __setitem__(self, *args):
-        raise AttributeError('objects are immutable: "%s".' % self.__class__.__name__)
--- sasha/tools/systemtools/__init__.py
+++ sasha/tools/systemtools/__init__.py
@@ -1,6 +1,5 @@
 from Bootstrap import Bootstrap
 from Fixture import Fixture
-from ImmutableDictionary import ImmutableDictionary
 from SashaConfiguration import SashaConfiguration
 
 _documentation_section = 'core'
