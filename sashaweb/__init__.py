dfdd691002e1be76ffe237412a70216274e45f2f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-19 11:41:05 -0700

    Modularized sashaweb root initializer.

--- sashaweb/__init__.py
+++ sashaweb/__init__.py
@@ -1,25 +1,16 @@
 from pyramid.config import Configurator
 from pyramid.httpexceptions import HTTPNotFound
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
-
 from sashaweb.views import *
 
 
-def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-
-    session_factory = UnencryptedCookieSessionFactoryConfig('hobbeshobbertson')
-    config = Configurator(settings=settings, session_factory=session_factory)
-
+def includeme(config):
     config.include('pyramid_mako')
-
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_static_view('assets/mp3s', 'sashamedia:development/mp3s/', cache_max_age=3600)
     config.add_static_view('assets/plots', 'sashamedia:development/plots/', cache_max_age=3600)
     config.add_static_view('assets/scores', 'sashamedia:development/scores/', cache_max_age=3600)
     config.add_static_view('docs', 'sasha:docs/build/html/', cache_max_age=3600)
-
     config.add_route('home', '/')
     config.add_route('help', '/help/')
     config.add_route('all_clusters', '/clusters/')
@@ -29,7 +20,13 @@ def main(global_config, **settings):
     config.add_route('single_event', '/events/{md5}/')
     config.add_route('single_instrument', '/instruments/{instrument_name}/')
     config.add_route('single_fingering', '/instruments/{instrument_name}/{compact_representation}/')
+    config.scan('sashaweb.views')
 
-    config.scan()
 
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    session_factory = UnencryptedCookieSessionFactoryConfig('hobbeshobbertson')
+    config = Configurator(settings=settings, session_factory=session_factory)
+    config.include(includeme)
     return config.make_wsgi_app()
