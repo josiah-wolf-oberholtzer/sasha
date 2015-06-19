76930483c153243b553975c956813c7c7b0abf1c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-19 11:31:13 -0700

    Added one view test.

---
+++ sashaweb/views/test/test_HomeView.py
@@ -0,0 +1,19 @@
+from pyramid import testing
+import unittest
+from sashaweb.views import HomeView
+
+
+class ViewTests(unittest.TestCase):
+
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_HomeView_01(self):
+        request = testing.DummyRequest()
+        view = HomeView(request)
+        info = view()
+        self.assertEqual(info['body_class'], 'home')
+        self.assertEqual(info['title'], 'SASHA | Home')
