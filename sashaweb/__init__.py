7832fd4137054ca82f52893db81187cb5283dc77
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-19 14:13:49 -0700

    Removed AllClustersView.

--- sashaweb/__init__.py
+++ sashaweb/__init__.py
@@ -1,25 +1,22 @@
 from pyramid.config import Configurator
-from pyramid.httpexceptions import HTTPNotFound
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
-from sashaweb.views import *
 
 
 def includeme(config):
     config.include('pyramid_mako')
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('help', '/help/')
+    config.add_route('home', '/')
+    config.add_route('random_event', '/random/')
+    config.add_route('search', '/search/')
+    config.add_route('single_cluster', '/clusters/{feature}/{cluster_id}/')
+    config.add_route('single_event', '/events/{md5}/')
+    config.add_route('single_fingering', '/instruments/{instrument_name}/{compact_representation}/')
+    config.add_route('single_instrument', '/instruments/{instrument_name}/')
     config.add_static_view('assets/mp3s', 'sashamedia:development/mp3s/', cache_max_age=3600)
     config.add_static_view('assets/plots', 'sashamedia:development/plots/', cache_max_age=3600)
     config.add_static_view('assets/scores', 'sashamedia:development/scores/', cache_max_age=3600)
     config.add_static_view('docs', 'sasha:docs/build/html/', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('help', '/help/')
-    config.add_route('all_clusters', '/clusters/')
-    config.add_route('single_cluster', '/clusters/{feature}/{cluster_id}/')
-    config.add_route('random_event', '/random/')
-    config.add_route('search', '/search/')
-    config.add_route('single_event', '/events/{md5}/')
-    config.add_route('single_instrument', '/instruments/{instrument_name}/')
-    config.add_route('single_fingering', '/instruments/{instrument_name}/{compact_representation}/')
+    config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan('sashaweb.views')
 
 
