5eaf1b8a4e17598c912375dc49048f57c98aadc1
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-25 04:23:40 +0100

    Implemented __format__ and __repr__ on Partial.

--- sasha/tools/newdomaintools/Partial.py
+++ sasha/tools/newdomaintools/Partial.py
@@ -9,3 +9,33 @@ class Partial(mongoengine.EmbeddedDocument):
     octave_number = mongoengine.FloatField()
     pitch_class_number = mongoengine.FloatField()
     pitch_number = mongoengine.FloatField()
+
+    ### SPECIAL METHODS ###
+
+    def __repr__(self):
+        from abjad.tools import systemtools
+        return systemtools.StorageFormatManager.get_repr_format(self)
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _repr_specification(self):
+        from abjad.tools.topleveltools import new
+        return new(
+            self._storage_format_specification,
+            is_indented=False,
+            )
+
+    @property
+    def _storage_format_specification(self):
+        from abjad.tools import systemtools
+        return systemtools.StorageFormatSpecification(
+            self,
+            is_bracketed=True,
+            keyword_argument_names=[
+                'amplitude',
+                'octave_number',
+                'pitch_class_number',
+                'pitch_number',
+                ],
+            )
