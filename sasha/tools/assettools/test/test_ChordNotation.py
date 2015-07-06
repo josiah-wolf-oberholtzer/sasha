f4a06439b0fc90231369cb85d118d7eb2d5f6c9a
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-07-06 16:25:26 -0700

    Added notation asset path tests.

--- sasha/tools/assettools/test/test_ChordNotation.py
+++ sasha/tools/assettools/test/test_ChordNotation.py
@@ -1,3 +1,4 @@
+import os
 from sasha import sasha_configuration
 from sasha.tools import assettools
 from sasha.tools import domaintools
@@ -13,3 +14,18 @@ def test_ChordNotation_01():
     assert not plugin.exists
     plugin.write()
     assert plugin.exists
+
+
+def test_ChordNotation_02():
+    event_a = domaintools.Event.get_one(id=1)
+    event_b = domaintools.Event.get_one(id=2)
+    analysis_a = assettools.ChordNotation(event_a)
+    analysis_b = assettools.ChordNotation(event_b)
+    assert analysis_a.path == os.path.join(
+        sasha_configuration.get_media_path('scores'),
+        'event__{}__chord.png'.format(event_a.md5),
+        )
+    assert analysis_b.path == os.path.join(
+        sasha_configuration.get_media_path('scores'),
+        'event__{}__chord.png'.format(event_b.md5),
+        )
--- sasha/tools/assettools/test/test_ChromaNotation.py
+++ sasha/tools/assettools/test/test_ChromaNotation.py
@@ -1,3 +1,4 @@
+import os
 from sasha import sasha_configuration
 from sasha.tools import assettools
 from sasha.tools import domaintools
@@ -13,3 +14,18 @@ def test_ChromaNotation_01():
     assert not plugin.exists
     plugin.write()
     assert plugin.exists
+
+
+def test_ChromaNotation_02():
+    event_a = domaintools.Event.get_one(id=1)
+    event_b = domaintools.Event.get_one(id=2)
+    analysis_a = assettools.ChromaNotation(event_a)
+    analysis_b = assettools.ChromaNotation(event_b)
+    assert analysis_a.path == os.path.join(
+        sasha_configuration.get_media_path('scores'),
+        'event__{}__chroma.png'.format(event_a.md5),
+        )
+    assert analysis_b.path == os.path.join(
+        sasha_configuration.get_media_path('scores'),
+        'event__{}__chroma.png'.format(event_b.md5),
+        )
