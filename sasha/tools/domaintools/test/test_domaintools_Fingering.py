98dc1af4e6bbb6ac480816f05199a16d215cfb03
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-07-08 13:17:06 -0700

    Wrote Fingering tests.

--- sasha/tools/domaintools/test/test_domaintools_Fingering.py
+++ sasha/tools/domaintools/test/test_domaintools_Fingering.py
@@ -1,7 +1,65 @@
-from sasha import *
+# -*- encoding: utf-8 -*-
+from pyramid import testing
+import sasha
+import unittest
 
 
-sasha_configuration.environment = 'testing'
+class FingeringTests(unittest.TestCase):
 
-def test_Fingering_01():
-    fingering = Fingering()
+    def setUp(self):
+        sasha.sasha_configuration.environment = 'testing'
+        self.config = testing.setUp()
+        self.config.include('sasha')
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_Fingering_01(self):
+        event = sasha.domaintools.Event.get_one(id=1)
+        fingering = event.fingering
+        self.assertEqual(fingering.instrument.name, 'Alto Saxophone')
+        self.assertEqual(fingering.compact_representation, '0010000000001010001110000')
+
+    def test_find_similar_fingerings(self):
+        event = sasha.domaintools.Event.get_one(id=1)
+        fingering = event.fingering
+        similar_fingerings = fingering.find_similar_fingerings()
+        self.assertEqual(len(similar_fingerings), 9)
+
+    def test_get_url(self):
+        event = sasha.domaintools.Event.get_one(id=1)
+        fingering = event.fingering
+        request = testing.DummyRequest(
+            matchdict={
+                'instrument_name': fingering.instrument.name,
+                'compact_representation': fingering.compact_representation,
+                },
+            )
+        url = fingering.get_url(request)
+        self.assertEqual(
+            url,
+            'http://example.com/instruments/alto-saxophone/0010000000001010001110000/',
+            )
+
+    def test_get_link(self):
+        event = sasha.domaintools.Event.get_one(id=1)
+        fingering = event.fingering
+        request = testing.DummyRequest(
+            matchdict={
+                'instrument_name': fingering.instrument.name,
+                'compact_representation': fingering.compact_representation,
+                },
+            )
+        link = fingering.get_link(request)
+        self.assertEqual(
+            link.encode('utf-8'),
+            '<a href="http://example.com/instruments/alto-saxophone/0010000000001010001110000/">Bf Ef L1 R1 R2 R3</a>',
+            )
+
+    def test_name(self):
+        event = sasha.domaintools.Event.get_one(id=1)
+        fingering = event.fingering
+        self.assertEqual(
+            fingering.name,
+            'Bf Ef L1 R1 R2 R3',
+            )
